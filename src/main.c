daa0d5a3387590e9a101486aa29b911f605792f0
saitoi <pedrohhs@dcc.ufrj.br>
2024-11-01 00:49:30 -0300

    simplificando main

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 
+// TODO: Distribuir melhor as funções, evitar chamar todos os headers
 #include "../include/processo.h"
 #include "../include/fila.h"
 #include "../include/escalonador.h"
@@ -9,35 +10,25 @@
 #include "../include/auxiliar.h"
 
 int main(void) {
-    Processo *processos = inicializa_processos(MAXIMO_PROCESSOS);
-
-    if (processos == NULL) {
-        tratar_erro_alocacao("Erro ao alocar processos iniciais.\n");
-        exit(1);
-    }
-
-    Fila fila_alta_prioridade, fila_baixa_prioridade, fila_disco, fila_fita, fila_impressora;
 
     srand(time(NULL));
+
+    Fila fila_alta_prioridade, fila_baixa_prioridade, fila_disco, fila_fita;
 
     inicializa_fila(&fila_alta_prioridade);
     inicializa_fila(&fila_baixa_prioridade);
     inicializa_fila(&fila_disco);
     inicializa_fila(&fila_fita);
-    inicializa_fila(&fila_impressora);
 
-    processa_menu(processos,
-                    &fila_alta_prioridade,
+    processa_menu(&fila_alta_prioridade,
                     &fila_baixa_prioridade,
                     &fila_disco, 
-                    &fila_fita, 
-                    &fila_impressora);
+                    &fila_fita);
 
     esvazia_fila(&fila_alta_prioridade);
     esvazia_fila(&fila_baixa_prioridade);
     esvazia_fila(&fila_disco);
     esvazia_fila(&fila_fita);
-    esvazia_fila(&fila_impressora);
 
     return 0;
 }
